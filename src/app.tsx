2123a7dbd65c14dd66b32b358f59eb8d08df49af
Prosper Osagie-Amayo <prosperosagieamayo@gmail.com>
2018-07-20 19:11:34 -0700

    initial commit

---
+++ src/app.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+class App extends React.Component<{}, {}> {
+  public render() {
+    return (
+      <div>
+        <p>Grapevine PD Admin Console</p>
+      </div>
+    );
+  }
+}
+
+export default App;
