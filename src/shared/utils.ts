2ac9e11708446ad84db4cba37258c79b05b7ef0f
Prosper Osagie-Amayo <prosperosagieamayo@gmail.com>
2018-07-24 19:30:49 -0700

    add: generate random string

---
+++ src/shared/utils.ts
@@ -0,0 +1,6 @@
+export const randStr = (length: number): string => {
+  const chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  let result = '';
+  for (let i = length; i > 0; --i) result += chars[Math.floor(Math.random() * chars.length)];
+  return result;
+};
